393c20c85674b811e234b819b5b0781c4209339e
IsolatedRain <3810173@qq.com>
2020-10-23 20:16:09 +0800

    init

---
+++ leetCode/1.两数之和/twoSum.go
@@ -0,0 +1,21 @@
+package leetCode
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	d := make(map[int]int, 0)
+	for i, num := range nums {
+		j, exist := d[target-num]
+		if exist {
+			return []int{j, i}
+		}
+		d[num] = i
+	}
+	return []int{}
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println(twoSum(nums, target))
+}
---
+++ leetCode/13. 罗马数字转整数/romanToInt.go
@@ -0,0 +1,33 @@
+package leetCode
+
+import "fmt"
+
+func romanToInt(s string) int {
+	dic := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000}
+	res := 0
+	pre := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		cur := dic[s[i]]
+		if cur >= pre {
+			res += cur
+		} else if cur < pre {
+			res -= cur
+		}
+		pre = cur
+	}
+	return res
+}
+
+func main() {
+	s := "LVIII"
+	s1 := "MCMXCIV"
+	fmt.Println(romanToInt(s))
+	fmt.Println(romanToInt(s1))
+}
