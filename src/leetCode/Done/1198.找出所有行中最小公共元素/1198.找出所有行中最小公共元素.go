23d6b103a9649d9564895c9a02de5df3757c1f60
IsolatedRain <3810173@qq.com>
2020-11-28 00:34:27 +0800

    "逐行更新列索引，直到所有行的 列索引位置值相等"

--- src/leetCode/Done/1198.找出所有行中最小公共元素/1198.找出所有行中最小公共元素.go
+++ src/leetCode/Done/1198.找出所有行中最小公共元素/1198.找出所有行中最小公共元素.go
@@ -2,34 +2,64 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	_ "sort"
 )
 
 func smallestCommonElement(mat [][]int) int {
 	n := len(mat)
-	matCnt := map[int]int{}
-	for r := range mat {
-		for c := range mat[r] {
-			matCnt[mat[r][c]]++
+	m := len(mat[0])
+	// 每一行的当前索引
+	ids := make([]int, n, n)
+	// 当前索引 至少 >= 的数
+	curVal := mat[0][0]
+	for {
+		tmp := curVal
+		for i := 0; i < n; i++ {
+			for mat[i][ids[i]] < curVal {
+				ids[i]++
+				if ids[i] >= m {
+					return -1
+				}
+			}
+			curVal = max(curVal, mat[i][ids[i]])
+		}
+		if curVal == tmp {
+			break
 		}
 	}
-	minCommon := []int{}
-	for k, v := range matCnt {
-		if v == n {
-			minCommon = append(minCommon, k)
-		}
+	return curVal
+}
+
+// n := len(mat)
+// matCnt := map[int]int{}
+// for r := range mat {
+// 	for c := range mat[r] {
+// 		matCnt[mat[r][c]]++
+// 	}
+// }
+// minCommon := []int{}
+// for k, v := range matCnt {
+// 	if v == n {
+// 		minCommon = append(minCommon, k)
+// 	}
+// }
+// if len(minCommon) < 1 {
+// 	return -1
+// }
+// sort.Slice(minCommon, func(i, j int) bool {
+// 	return minCommon[i] < minCommon[j]
+// })
+// return minCommon[0]
+// }
+func max(x, y int) int {
+	if x > y {
+		return x
 	}
-	if len(minCommon) < 1 {
-		return -1
-	}
-	sort.Slice(minCommon, func(i, j int) bool {
-		return minCommon[i] < minCommon[j]
-	})
-	return minCommon[0]
+	return y
 }
 
 func main() {
-	mat := [][]int{{1, 2, 3, 4, 5}, {2, 4, 5, 8, 10}, {3, 5, 7, 9, 11}, {1, 3, 5, 7, 9}}
+	mat := [][]int{{1, 2, 3, 4, 5}, {2, 4, 6, 8, 10}, {3, 5, 7, 9, 11}, {1, 3, 5, 7, 9}}
 	r := smallestCommonElement(mat)
 	fmt.Println(r)
 }
